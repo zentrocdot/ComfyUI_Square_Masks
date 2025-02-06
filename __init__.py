0f0d05aace8ca520f924036382b7d44c9c247129
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-06 13:41:14 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
-from .nodes.circle_mask import *
+from .nodes.nodes import *
+from .nodes.showdatanodes import *
+from .nodes.inputdatanodes import *
 
 NODE_CLASS_MAPPINGS = { 
-    "🔬 Circle Masks": CircleMasks,
+    "🔬 Circle Detection": CircleDetection,
+    "📄 Show Data": ShowData,
+    "✏️ Input Data": InputData,
     }
     
 WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
 
-MESSAGE = "\033[34mComfyUI Square Masks Nodes: \033[92mLoaded\033[0m" 
-
-print(MESSAGE)
+print("\033[34mComfyUI Circle Detection Nodes: \033[92mLoaded\033[0m")
